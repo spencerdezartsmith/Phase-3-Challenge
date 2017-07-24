33d1858ab6cd0ce04482c2fdc7c9b0556a60d021
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-07-24 11:07:01 -0700

    Add square route

--- part-1/routes/index.js
+++ part-1/routes/index.js
@@ -2,7 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/supported-operations', (req, res) => {
-  res.send({ hello: "world" })
+  res.send({
+    "/": "division",
+    "+": "addition",
+    "-": "subtration",
+    "*": "multiplication"
+  })
+})
+
+router.get('/square', (req, res) => {
+  const number = Number(req.query.number)
+  const squared = number * number
+  res.send({ result: squared })
 })
 
 module.exports = router
