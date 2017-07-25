3dc7b18eb698f1c44e0a3fe9e7e943c82d0a739f
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-07-25 15:13:25 -0700

    Add open and close functionality to modal

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,9 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+
   // Jump to section
   const sections = document.querySelector('.section-list')
   sections.addEventListener('click', (event) => {
     window.location.hash = event.target.textContent
   })
 
-  
+  // Open and close modal
+  const modal = document.getElementById('modal')
+  const overlay = document.getElementById('modal-overlay')
+  const closeBtn = document.getElementById('close-btn')
+  const cartBtn = document.querySelector('#cart-button')
+  // Open
+  cartBtn.addEventListener('click', () => {
+	  modal.classList.remove('modal-close')
+	  overlay.classList.remove('modal-close')
+  })
+  // close
+  closeBtn.addEventListener('click', () => {
+	  overlay.classList.add('modal-close')
+	  modal.classList.add('modal-close')
+  })
+
 })
